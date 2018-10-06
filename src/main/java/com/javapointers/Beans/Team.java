b48eb9a41b1ffffac5820bc389694ca1381de709
PSEH-8 <39406190+PSEH-8@users.noreply.github.com>
2018-10-06 14:56:31 +0530

    Update Team.java

--- src/main/java/com/javapointers/Beans/Team.java
+++ src/main/java/com/javapointers/Beans/Team.java
@@ -1,5 +1,73 @@
 package com.javapointers.Beans;
 
-public class Team {
+import java.io.Serializable;
+
+public class Team implements Serializable {
+
+
+    private String action;
+    private String country_id;
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(String country_id) {
+        this.country_id = country_id;
+    }
+
+    public String getLeague_id() {
+        return league_id;
+    }
+
+    public void setLeague_id(String league_id) {
+        this.league_id = league_id;
+    }
+
+    public String getMatch_id() {
+        return match_id;
+    }
+
+    public void setMatch_id(String match_id) {
+        this.match_id = match_id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    private String league_id;
+    private String match_id;
+    private String from;
+    private String name;
+    private String to;
 
 }
